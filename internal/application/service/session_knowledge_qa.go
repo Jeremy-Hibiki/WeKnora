e650de92f4ce06f5466299669785656523c678a5
Binks Riverton <yuhao615@yeah.net>
2026-07-08 13:05:16 +0800

    chore: remove debug logs from folder search integration

--- internal/application/service/session_knowledge_qa.go
+++ internal/application/service/session_knowledge_qa.go
@@ -99,9 +99,6 @@ func (s *sessionService) KnowledgeQA(
 			st.IncludeSubfolders = req.IncludeSubfolders
 		}
 	}
-	logger.Infof(ctx, "[FolderScope] KnowledgeQA: FolderIDs=%v, IncludeSubfolders=%v, searchTargets=%d",
-		req.FolderIDs, req.IncludeSubfolders, len(searchTargets))
-
 	// Create chat management object with session settings
 	logger.Infof(
 		ctx,
